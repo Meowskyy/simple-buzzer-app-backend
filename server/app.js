e90794f967f49c212076ff1689644c2470d9db0f
Meowskyy <ruube.leynbaum@gmail.com>
2019-12-22 21:55:01 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,19 +13,21 @@ const io = socketIo(server); // < Interesting!
 
 const getApiAndEmit = async socket => {
     try {
-      socket.emit("FromAPI", "U gay"); // Emitting a new message. It will be consumed by the client
+      i++
+      socket.emit("FromAPI", "U gay" + i); // Emitting a new message. It will be consumed by the client
     } catch (error) {
       console.error(`Error: ${error.code}`);
     }
 };
 
 let interval;
+let i = 0;
 io.on("connection", socket => {
   console.log("New client connected");
   if (interval) {
     clearInterval(interval);
   }
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  interval = setInterval(() => { getApiAndEmit(socket), 10000; });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
