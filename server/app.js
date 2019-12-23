924d55751cb77d51f923e22f8512b7bbaacf5963
Meowskyy <ruube.leynbaum@gmail.com>
2019-12-23 23:18:59 +0200

    Basic functions implemented

--- server/app.js
+++ server/app.js
@@ -11,26 +11,48 @@ const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-const getApiAndEmit = async socket => {
-    try {
-      i++
-      socket.emit("FromAPI", "U gay" + i); // Emitting a new message. It will be consumed by the client
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-};
+var players = [];
 
-let interval;
-let i = 0;
 io.on("connection", socket => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => { getApiAndEmit(socket), 10000; });
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
+  console.log("New client connected: " + socket.id);
+  players.push({id: socket.id, name: "None" });
+
+  socket.emit('players', players);
+
+  socket.broadcast.emit('player-new-connected', socket.id);
+  
+  socket.on ('player-rename', data => {
+    console.log(`Player ${data.id} renamed to: ${data.name}`);
+    socket.broadcast.emit ('player-rename', data);
   });
+
+  socket.on('player-hate', player => {
+    console.log(`${player.name} hates it!`);
+    socket.broadcast.emit('player-hate', player);
+  })
+
+  socket.on('player-love', player => {
+    console.log(`${player.name} loves it!`);
+    socket.broadcast.emit('player-love', player);
+  })
+  
+  socket.on("disconnect", () => {
+    var playerIndex = -1;
+    for (var i = 0; i < players.length; i++) {
+      if (socket.id === players[i].id) {
+        playerIndex = i;
+        break;
+      }
+    }
+
+    if (playerIndex > -1) {
+      players.splice(playerIndex, 1);
+    }
+
+    console.log("Client disconnected");
+    socket.broadcast.emit('player-disconnected', socket.id);
+  });
+
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
