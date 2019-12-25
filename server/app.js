828808604cbf87a30b4e18c94c586333f381e414
Meowskyy <ruube.leynbaum@gmail.com>
2019-12-25 23:20:27 +0200

    Update states

--- server/app.js
+++ server/app.js
@@ -9,9 +9,21 @@ app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server);
 
 var players = [];
+
+function updatePlayerState(player ) {
+    console.log("Update state");
+
+    for (var i = 0; i < players.length; i++) {
+        if (player.id === players[i].id) {
+            console.log("Found player");
+            players[i] = player;
+            break;
+        }
+    }
+}
 
 io.on("connection", socket => {
   console.log("New client connected: " + socket.id);
@@ -21,26 +33,26 @@ io.on("connection", socket => {
 
   socket.broadcast.emit('player-new-connected', socket.id);
   
-  socket.on ('player-rename', data => {
-    console.log(`Player ${data.id} renamed to: ${data.name}`);
-    socket.broadcast.emit ('player-rename', data);
+  socket.on ('player-rename', player => {
+    console.log(`Player ${player.id} renamed to: ${player.name}`);
+
+    updatePlayerState(player);
+
+    socket.broadcast.emit ('player-rename', player);
   });
 
   socket.on('player-hate', player => {
     console.log(`${player.name} hates it!`);
+
+    updatePlayerState(player);
+
     socket.broadcast.emit('player-hate', player);
   })
 
   socket.on('player-love', player => {
     console.log(`${player.name} loves it!`);
 
-    for (var i = 0; i < players.length; i++) {
-        if (player.id === players[i].id) {
-            console.log("Found player");
-            players[i] = player;
-            break;
-        }
-    }
+    updatePlayerState(player);
 
     socket.broadcast.emit('player-love', player);
   })
